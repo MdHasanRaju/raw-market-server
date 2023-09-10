1251a032cda0789b17e2c9719459eb14b3d0f118
MdHasanRaju <mdhasanraju2017@gmail.com>
2023-09-10 13:29:23 +0600

    :fire: uri updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
-const colors = require("colors"); 
+const colors = require("colors");
 require("dotenv").config();
 
 const PORT = 3000;
@@ -14,10 +14,13 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
+const URI = process.env.MONGO_URI;
+
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.qkvdpja.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
-  )
+  //   .connect(
+  //     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@atlascluster.qkvdpja.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  //   )
+  .connect(URI)
   .then(() => {
     console.log(`Database connection is successful 🛢`.green.bold);
   });
